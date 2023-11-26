ca590fb6a32cf4f6f0faae4dcada0fb5fc839cf2
piotrbosowski <piotr.bosowski@gmail.com>
2023-11-26 16:46:28 +0100

    Add comments, simplify CharCoder

--- slowoku/charcoder.py
+++ slowoku/charcoder.py
@@ -1,18 +1,43 @@
+"""
+This module implements the interface between human-readable strings and their
+integer-based representation through the CharCoder class.
+"""
+
+
 import numpy as np
 
 
-class CharCoder:
-    # char_pool: set
-    char_map: dict
-    char_map_inv: dict
+class UnknownCharacter(Exception):
+    """
+    This exception is raised when .decode() meets a character that this
+    instance of CharCoder has not seen before.
+    """
+    pass
 
+
+class CharCoder:
+    """
+    CharCoder is a class that allows to encode a sequence of incoming strings
+    without prior knowledge about the set of characters. Every time a new
+    character occurs, it is mapped to a unique integer value. Moreover, the
+    class also maintains the reverse mapping to enable decoding in the same
+    manner.
+    """
     def __init__(self):
-        # self.char_pool = set()
         self.char_map = dict()
         self.char_map_inv = dict()
         self.next_code = 1
 
     def encode(self, word: str) -> np.ndarray:
+        """
+        The function takes a string (a word) and returns its numerical
+        (integer-based) representation: a sequence of ints (np.ndarray).
+        If the new word have some characters unmet before, the class
+        gracefully creates new codes for these letters. There are also
+        no restrictions regarding the length of the word.
+        :param word: string-represented word to be encoded
+        :return: 1-dimensional np.ndarray of letter-codes
+        """
         chars = set(word)
         for char in chars:
             if char not in self.char_map:
@@ -22,5 +47,16 @@ class CharCoder:
         return np.array(list(map(lambda c: self.char_map[c], word)),
                         dtype=np.int8)
 
-    def decode(self, word: np.ndarray):
-        return ''.join(list(map(lambda ind: self.char_map_inv[ind], word)))
+    def decode(self, coded_word: np.ndarray):
+        """
+        The function decodes a given int-array-represented word back to the
+        string representation. It is important to notice that, when decoding
+        a word, the encoder must have seen all its characters previously
+        (otherwise the UnknownCharacter error will be thrown).
+        :param coded_word: np.ndarray representing a word to decode
+        :return: string-based representation of the word
+        """
+        try:
+            return ''.join(list(map(lambda ind: self.char_map_inv[ind], coded_word)))
+        except KeyError:
+            raise UnknownCharacter
--- slowoku/game_engine.py
+++ slowoku/game_engine.py
@@ -15,27 +15,25 @@ import numpy as np
 
 
 class Engine:
-    char_dict = {0: '-',
-                 1: 'y',
-                 2: 'g'}
+    HUMAN_REPR = {0: '-',
+                  1: 'y',
+                  2: 'g'}
 
-    def get_color_hint(self, word: np.ndarray, secret: np.ndarray):
-        repeating = np.intersect1d(word, secret)
+    def get_color_hint(self,
+                       guess: np.ndarray,
+                       secret: np.ndarray,
+                       human_reable: bool = False):
+        repeating = np.intersect1d(guess, secret)
         # green assignments:
-        greens = word == secret  # immediately give green where chars match
+        greens = guess == secret  # immediately give green where chars match
         # then count, how many yellow letters should still be considered:
         counting = np.array([np.count_nonzero((secret == letter) & ~greens)
                              for letter in repeating])
         yellows = np.zeros_like(greens)
         for letter, count in zip(repeating, counting):
-            # take count most-left occurances of the yellow letter
-            occurances = np.argwhere(word * ~greens == letter)[:count]
-            yellows[occurances] = True
+            # take count most-left occurrences of the yellow letter
+            occurrences = np.argwhere(guess * ~greens == letter)[:count]
+            yellows[occurrences] = True
         output = np.zeros_like(greens) + yellows + 2 * greens
-        return ''.join(self.char_dict[c] for c in output)
-#
-#
-# if __name__ == '__main__':
-#     cc = CharCoder()
-#     output = Engine().get_color_hint(cc.encode('banan'), cc.encode('wanna'))
-#     print(output)
+        return ''.join(self.HUMAN_REPR[c] for c in output) \
+            if human_reable else output
