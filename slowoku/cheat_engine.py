cd320ec13cf903993c059d725496d3917637ef64
piotrbosowski <piotr.bosowski@gmail.com>
2023-12-21 14:40:36 +0100

    Start parallel approach to eliminating results

--- slowoku/cheat_engine.py
+++ slowoku/cheat_engine.py
@@ -3,10 +3,11 @@ import numpy as np
 
 class CheatEngine:
     letter_rules = []
-    valid_words = []
+    wordlist = []
 
     def __init__(self, wordlist):
-        self.valid_words = np.copy(wordlist)
+        self.wordlist = wordlist
+        self.valid_mask = np.ones(len(wordlist), dtype=bool)
 
     @staticmethod
     def colored_letter_counter(guess, result, color):
@@ -20,26 +21,32 @@ class CheatEngine:
         b_letter_counts = self.colored_letter_counter(guess, result, color=0)
         # apply green:
         if np.any(result == 2):
-            green_ok_mask = \
-                np.all((guess == self.valid_words)[:, result == 2], axis=1)
-            self.valid_words = self.valid_words[green_ok_mask]
+            cumulative_mask = \
+                np.all((guess == self.wordlist)[:, result == 2], axis=1)
+            # self.wordlist = self.wordlist[green_ok_mask]
+        else:
+            cumulative_mask = np.ones(len(self.wordlist), dtype=bool)
         # apply yellow:
         for y_letter, y_count in y_letter_counts.items():
             threshold = y_count + (g_letter_counts[y_letter]
                                    if y_letter in g_letter_counts else 0)
-            yellow_ok_mask = np.sum((self.valid_words == y_letter),
+            yellow_ok_mask = np.sum((self.wordlist[cumulative_mask] == y_letter),
                                     axis=1) >= threshold
-            self.valid_words = self.valid_words[yellow_ok_mask]
+            cumulative_mask[cumulative_mask] = yellow_ok_mask
+            # self.wordlist = self.wordlist[yellow_ok_mask]
             # now also delete these words where y_letter is on its position:
-            yellow_2nd_mask = np.all((self.valid_words != guess)
+            yellow_2nd_mask = np.all((self.wordlist[cumulative_mask] != guess)
                                      [:, result == 1], axis=1)
-            self.valid_words = self.valid_words[yellow_2nd_mask]
+            # self.wordlist = self.wordlist[yellow_2nd_mask]
+            cumulative_mask[cumulative_mask] = yellow_2nd_mask
         # apply black:
         for b_letter, _ in b_letter_counts.items():
             threshold = (g_letter_counts[b_letter]
                          if b_letter in g_letter_counts else 0) + \
                         (y_letter_counts[b_letter]
                          if b_letter in y_letter_counts else 0)
-            black_ok_mask = np.sum((self.valid_words == b_letter),
+            black_ok_mask = np.sum((self.wordlist[cumulative_mask] == b_letter),
                                    axis=1) <= threshold
-            self.valid_words = self.valid_words[black_ok_mask]
+            # self.wordlist = self.wordlist[black_ok_mask]
+            cumulative_mask[cumulative_mask] = black_ok_mask
+        self.valid_mask = cumulative_mask
